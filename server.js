0de0fe5a376df83862decf52e1d77c1853f21036
y4donts4y <stella.l.griffith@gmail.com>
2018-12-21 23:00:35 -0500

    updated server to connect to handlebars and routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require("express");
-
 var app = express();
+var PORT = process.env.PORT || 5000;
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+// Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log("Server up: http://localhost:" + PORT);
