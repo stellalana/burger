fc77027347e5d03566cb504e5f9a219b2de68344
y4donts4y <stella.l.griffith@gmail.com>
2018-12-21 22:36:53 -0500

    added model code that calls orm methods

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+// Import ORM in order to use it's methods
+var orm = require("../config/orm.js");
+
+// Calls ORM functions
+var burger = {
+    selectAll: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (burgers_controller.js).
+  module.exports = burger;
