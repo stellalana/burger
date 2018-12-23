310b5fd99ba3310ffa6a90f825e659e7421f4832
y4donts4y <stella.l.griffith@gmail.com>
2018-12-22 19:25:50 -0500

    heroku fix attempt 2

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var express = require("express");
-var PORT = process.env.PORT || 5000;
-var app = express();
 
+var express = require("express");
+var app = express(); 
+var PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Handlebars.
-var exphbs = require("express-handlebars");
 
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
+
 app.listen(PORT, function() {
-  console.log("Server up: http://localhost:" + PORT);
+  console.log("Server up at localhost:" + PORT);
 });
